b606c609f441e4cd63533dab934510d1e3366c76
anna97490 <anna.cousin97490@gmail.com>
2025-08-01 11:27:59 +0200

    implement secure reservation API with response DTOs and verifications

--- src/test/java/com/mastere_project/vacances_tranquilles/service/impl/ServiceServiceImplTest.java
+++ src/test/java/com/mastere_project/vacances_tranquilles/service/impl/ServiceServiceImplTest.java
@@ -22,7 +22,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
